ed3828fc9023dd90806edc438a50673699d4b554
German <german.fidarov@gmail.com>
2021-09-22 00:12:07 +0300

    Basic Solution
    Image Tracing without any other capabilities

---
+++ Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImageManager;
+
+    private void Awake()
+    {
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged; 
+    }
+
+    public void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged; 
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+}
